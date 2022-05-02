7c5b98a82ae6139b288c0b1380b58c432b499ba1
janaAdler <85888042+janaAdler@users.noreply.github.com>
2022-05-02 13:37:54 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,22 +1,36 @@
-
 import streamlit as st
 import pandas as pd
 import numpy as np
-st.title('Image Captioning Group 13')
+import json
+import jsonlines
+import glob
 
-with st.expander("Our Team"):
-  st.write("TEAM ")
-with st.expander("Our Goal"):
-  st.write("We want to classify tweets.")
-with st.expander("Our Data"):
-  st.write("Datasetexamples")
-  col1, col2, col3 = st.columns(3)
+#Present your team
+st.title('Political Party Classification')
 
+
+with st.expander("Our Team:"):
+    col1, col2, col3 = st.columns(3)
 with col1:
-    st.header("Chrysler Logo")
+    st.markdown(f'<h1 style="color:#000000;font-size:16px;">{"Jan Amend"}</h1>', unsafe_allow_html=True)
+    st.image("https://www.sprit-plus.de/sixcms/media.php/5172/thumbnails/Mann_Fragezeichen.jpg.31563300.jpg")
 
 with col2:
-    st.header("NIKE Shoe")
+    st.markdown(f'<h1 style="color:#000000;font-size:16px;">{"Jana Adler"}</h1>', unsafe_allow_html=True)
+    st.image("https://www.sprit-plus.de/sixcms/media.php/5172/thumbnails/Mann_Fragezeichen.jpg.31563300.jpg")
 
 with col3:
-    st.header("Girl in a white dress")
+    st.markdown(f'<h1 style="color:#000000;font-size:16px;">{"Sergei Mezhonnov"}</h1>', unsafe_allow_html=True)
+    st.image("https://scontent-frt3-2.xx.fbcdn.net/v/t31.18172-8/20451827_1559088087491677_5562632512013699296_o.jpg?_nc_cat=103&ccb=1-5&_nc_sid=09cbfe&_nc_eui2=AeEdsPUBJzyPOZFs7ylgC0hgD7d7OvNp0kMPt3s682nSQ3wLFAZVTOrmLSOyHzD2mTt9LJKE7h7ZjiAONvU_-HJN&_nc_ohc=6ihYdGJVRsYAX_5hGRe&_nc_ht=scontent-frt3-2.xx&oh=00_AT893UIInBTyBmNpf071lnSEZBuEZS3igGFkVQEABTd9rg&oe=6294469E")
+
+#present your project
+with st.expander("About Project:"):
+    st.text('Bla Bla Bla')
+#show one element of your dataset
+with st.expander('Example of dataset'):
+    with jsonlines.open('data/AfDProtschka.jl') as jl:
+        for item in jl:
+            for tweet in item['response']['data']:
+                st.write(tweet)
+                break
+            break
