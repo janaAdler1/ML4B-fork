876005f18a671ae3d6f6e1922e40409aab7d59bd
janaAdler <85888042+janaAdler@users.noreply.github.com>
2022-07-05 21:52:46 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,22 +14,22 @@ selected = option_menu(None, ["Home", "Dataset",  "Process", 'Live Demo', 'Blub'
 if selected=="Home":
     st.markdown("<h1 style='text-align: center'>Political Party Classification</h1>", unsafe_allow_html=True)
     st.markdown("<h2 style='text-align: center'>Our Goal</h2>", unsafe_allow_html=True)
-    st.markdown("<h4 style='text-align: center'>The goal of our project is to classify Tweets of german politicans by the political party of the author. However, we don't just want to research the politicians and cathegorize them manually, we want to use Machine Learning algorithms.</h4>", unsafe_allow_html=True)
+    st.markdown("<h6 style='text-align: center'>The goal of our project is to classify Tweets of german politicans by the political party of the author. However, we don't just want to research the politicians and cathegorize them manually, we want to use Machine Learning algorithms.</h6>", unsafe_allow_html=True)
     st.markdown("<h2 style='text-align: center'>Our Team</h2>", unsafe_allow_html=True)
 
     #present your team
     col1, col2, col3 = st.columns(3)
     with col1:
         st.image("image_jan.jpeg", caption = 'Jan Amend')
-        st.text("4th semester Wirtschaftsinformatik")
+        st.markdown("<h6 style='text-align: center'>4th semester Wirtschaftsinformatik</h6>", unsafe_allow_html=True)
         st.text("TEXT")
     with col2:
         st.image("image.jpeg", caption = "Jana Adler")
-        st.text("4th semester Wirtschaftsinformatik")
+        st.markdown("<h6 style='text-align: center'>4th semester Wirtschaftsinformatik</h6>", unsafe_allow_html=True)
         st.text("Hi, my name is Jana and I'm currently part of a dual studies programm at DATEV where im focusing on internet security. Since ML and AI is a pretty huge deal in web security I'm very interested in this topic. In my spare time I like to dance and go for a ride on my motorcycle.")
     with col3:
         st.image("image_ser.jpg", caption = 'Sergei Mezhonnov')
-        st.text("4th semester Wirtschaftsinformatik")
+        st.markdown("<h6 style='text-align: center'>4th semester Wirtschaftsinformatik</h6>", unsafe_allow_html=True)
         st.text("TEXT")
 
 if selected=="Dataset":
